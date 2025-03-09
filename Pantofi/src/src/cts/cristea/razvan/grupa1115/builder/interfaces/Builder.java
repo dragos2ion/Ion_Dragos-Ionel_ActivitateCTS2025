18169ccfd74addabcea19a82732f2b0714942365
dragosjon <107347132+dragosjon@users.noreply.github.com>
2025-03-09 16:19:37 +0200

    Third

--- Pantofi/src/src/cts/cristea/razvan/grupa1115/builder/interfaces/Builder.java
+++ Pantofi/src/src/cts/cristea/razvan/grupa1115/builder/interfaces/Builder.java
@@ -1,6 +1,6 @@
 package src.cts.cristea.razvan.grupa1115.builder.interfaces;
 
-import cts.cristea.razvan.grupa1115.builder.classes.Pantof;
+import src.cts.cristea.razvan.grupa1115.builder.classes.Pantof;
 
 public interface Builder {
     Pantof build() throws Exception;
